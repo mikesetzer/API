553c08acfedade9827f061678669eec68bde159b
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-26 14:45:24 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <div class="container-fluid p-md-5">
         <div class="d-flex align-items-center">
           <!-- Logo Image -->
-          <img src="https://scontent.ftpa1-2.fna.fbcdn.net/v/t39.30808-6/273118883_10159073885483096_3044435513138211141_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=FrE_rR8uQ1sAX_5-Xcz&_nc_ht=scontent.ftpa1-2.fna&oh=00_AfBHL5qqwqot6SrpDTUjFeYj-E3Wr2ontbsody2h5z65Hg&oe=653F261A" alt="Company Logo" class="logo me-3">
+          <img src="https://scontent.ftpa1-2.fna.fbcdn.net/v/t39.30808-6/273118883_10159073885483096_3044435513138211141_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=FrE_rR8uQ1sAX_5-Xcz&_nc_ht=scontent.ftpa1-2.fna&oh=00_AfBHL5qqwqot6SrpDTUjFeYj-E3Wr2ontbsody2h5z65Hg&oe=653F261A" alt="Fairwinds Logo" class="logo me-3">
           <!-- Title next to the logo -->
           <h1 class="display-4 py-3">Customer Management</h1>
         </div>
